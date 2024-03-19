5a51799b46dab82d068207e7e54cd421aafd797c
KJJ0513 <happyday960513@gmail.com>
2024-03-19 19:39:26 +0900

    임시 저장

--- ProjectHRD/src/main/java/com/bswill/service/NotificationServiceImpl.java
+++ ProjectHRD/src/main/java/com/bswill/service/NotificationServiceImpl.java
@@ -1,12 +1,18 @@
 package com.bswill.service;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Repository;
 
 import com.bswill.domain.NotificationVO;
+import com.bswill.persistence.NotificationDAO;
 
 @Repository
 public class NotificationServiceImpl implements NotificationService{
 
+	@Inject
+	private NotificationDAO ndao;
+	
 	@Override
 	public NotificationVO selectNoti(int employee_id) {
 		
