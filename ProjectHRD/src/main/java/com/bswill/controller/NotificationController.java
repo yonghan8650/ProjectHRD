bbdc83cfa1b504cfc5c845600733953eba134a22
KJJ0513 <happyday960513@gmail.com>
2024-03-21 17:58:10 +0900

    DB,매퍼,알림 서비스 설정 변경 및 임시 저장

--- ProjectHRD/src/main/java/com/bswill/controller/NotificationController.java
+++ ProjectHRD/src/main/java/com/bswill/controller/NotificationController.java
@@ -12,9 +12,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bswill.domain.NotificationVO;
 import com.bswill.service.NotificationService;
+import com.bswill.service.NotificationServiceImpl;
 
 @Controller
 @RequestMapping(value = "/*")
@@ -30,7 +32,7 @@ public class NotificationController {
 	// 알림 목록 조회
 	// http://localhost:8088/notifications
 	@RequestMapping(value = "/notifications",method = RequestMethod.GET)
-	public void listGet(/*Model model*/) throws Exception{
+	public void listGet(Model model) throws Exception{
 		logger.debug(" /notifications -> listGet() 실행");
 		logger.debug(" /notifications.jsp 연결 ");
 		// 서비스 -> DAO 알림 목록 가져오기
@@ -38,6 +40,32 @@ public class NotificationController {
 	
 		logger.debug(" list.size : "+ notiList.size());
 		// 연결된 뷰페이지에 정보 정달(Model)
-//		model.addAttribute("notiList",notiList);
+		model.addAttribute("notiList",notiList);
 	}
+	
+	// 알림 읽음
+	// 
+	@RequestMapping(value = "/readNoti",method = RequestMethod.GET)
+	public String readNotification(@RequestParam("employee_id")int employee_id,Model model)throws Exception {
+		logger.debug(" /readNoti -> read 실행 ");
+		logger.debug(" /readNoti.jsp 연결 ");
+		// 서비스 -> DAO 알림 읽음
+		try {
+            // 알림을 읽음
+            nService.read(employee_id);
+            
+		} catch (Exception e) {
+            // 예외 처리
+            e.printStackTrace();
+            // 예외 상황에 따른 처리를 할 수 있음
+        }
+        return "readNoti";
+		
+		
+		
+	}
+	
+	
+	
+	
 }
--- ProjectHRD/src/main/webapp/WEB-INF/views/notifications.jsp
+++ ProjectHRD/src/main/webapp/WEB-INF/views/notifications.jsp
@@ -6,11 +6,32 @@
 <meta charset="UTF-8">
 <title>Notifications</title>
 <style>
+div {
+border: solid;
 
+}
+
+td{
+padding : 20px;
+}
 </style>
 </head>
 <body>
 	<h3>Notifications</h3>
-
+	<div class="box-body">
+		<table class="table table-bordered">
+			<tbody>
+				<c:forEach var="nVO" items="${notiList }">
+					<tr>
+						<td>${nVO.employee_id }</td>
+						<td>${nVO.noti_title }</td>
+						<td>${nVO.noti_time }</td>
+						<td>${nVO.noti_link }</td>
+						<td><input type="button"></td>
+					</tr>
+				</c:forEach>
+			</tbody>
+		</table>
+	</div>
 </body>
 </html>
