62e705db88e9ee7c9f6179a8396d316e97c58be4
yonghan8650 <157337509+yonghan8650@users.noreply.github.com>
2024-03-27 16:03:36 +0900

    Update CommonController.java

--- ProjectHRD/src/main/java/com/bswill/controller/CommonController.java
+++ ProjectHRD/src/main/java/com/bswill/controller/CommonController.java
@@ -37,7 +37,6 @@ public class CommonController {
 		logger.info(" 접근 권한없는 접근 발생 ");
 		logger.info(" auth : " + auth);
 		
-		model.addAttribute("auth", auth);
 	}
 
 	// http://localhost:8088/common/customLogin
